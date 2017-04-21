f6e64b063b8785543c09b3baa8efbf686eec638a
timothy <tabradford@liberty.edu>
2017-04-21 13:50:05 -0400

    Added java docs

--- src/genericclass/GenericClass.java
+++ src/genericclass/GenericClass.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Creates an ArrayList of 10 Pair objects containing values. The program will
+ * then print the string representation of each Pair to System.out (i.e., use
+ * the toString() method).
  *
- * @author ^_^
+ * @author Timothy Bradford
  */
 public class GenericClass {
 
@@ -18,14 +21,15 @@ public class GenericClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
 
         List<Pair> x = new ArrayList<>();
 
+        //add Pair values to ArrayList
         for (int i = 0; i < 10; i++) {
             x.add(new Pair(1 + i, "Test: " + (i + 1)));
         }
 
+        //Print all Pair values toString methods
         for (int i = 0; i < x.size(); i++) {
             System.out.println(x.get(i).toString());
         }
--- src/genericclass/Pair.java
+++ src/genericclass/Pair.java
@@ -6,6 +6,8 @@
 package genericclass;
 
 /**
+ * A generic class Pair which has 2 type parameters, F and S, representing the
+ * type of the first and second elements of a pair respectively.
  *
  * @author Timothy Bradford
  * @param <F> generic type F
@@ -16,30 +18,59 @@ public class Pair<F, S> {
     public F f;
     public S s;
 
-    public Pair() {}
+    /**
+     * Default Constructor 
+     */
+    public Pair() {
+    }
 
+    /**
+     * 
+     * @param f generic value
+     * @param s generic value
+     */
     public Pair(F f, S s) {
         this.f = f;
         this.s = s;
     }
 
+    /**
+     *  method to display the first and second values of the pair 
+     * @return the first and second values of the pair in string format
+     */
     @Override
     public String toString() {
         return "Pair{" + "f=" + f + ", s=" + s + '}';
     }
 
+    /**
+     * 
+     * @return generic value
+     */
     public F getF() {
         return f;
     }
 
+    /**
+     * 
+     * @param f generic value
+     */
     public void setF(F f) {
         this.f = f;
     }
 
+    /**
+     * 
+     * @return generic value
+     */
     public S getS() {
         return s;
     }
 
+    /**
+     * 
+     * @param s generic value
+     */
     public void setS(S s) {
         this.s = s;
     }
